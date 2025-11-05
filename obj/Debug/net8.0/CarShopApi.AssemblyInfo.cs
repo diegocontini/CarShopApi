a2b89616680f9ac10acc18bb83fe98fb37b1a1cd
diego <diconediego@outlook.com>
2025-11-04 22:22:57 -0300

    chore: docs in readme

--- obj/Debug/net8.0/CarShopApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650bc9900b88dfa48f0e38f4ed185e76db18bba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6873cffe60d9a9d455bf85b9e36950eeb43a0556")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
+# Arquitetura
+Projeto desenvolvido utilizando .NET 8.0.
+Tanto a API quanto o banco de dados estão dockeirizados.
+
+## Controller
+Responsável por receber as requisições HTTP, chamar os serviços apropriados e retornar as respostas HTTP.
+
+## Service
+Responsável pela lógica de negócio da aplicação. Ele processa os dados recebidos dos controllers e interage com o repositório para acessar o banco de dados.
+
+## Model
+Define as entidades e estruturas de dados utilizadas na aplicação. Representa os objetos do domínio do negócio e suas respectivas tabelas no banco de dados.
+
+
+# EF Core
+```
 dotnet ef migrations add InitialMigration
 dotnet ef database update
+```
